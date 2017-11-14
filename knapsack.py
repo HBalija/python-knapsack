d914b2081a17ec641ba2cd62e745bf834ac92362
HBalija <balija.hrvoje@gmail.com>
2017-11-14 12:15:52 +0100

    Fix input method

--- knapsack.py
+++ knapsack.py
@@ -10,15 +10,12 @@ class Knapsack:
     _DATA_LENGTH_ERROR = 'Three lines required. File is {} lines long.'
     _DATA_INPUT_ERROR = 'Invalid input.'
 
-    def __init__(self, weight_available, weight_per_item, value_per_item):
+    def __init__(self, weight_available, number_of_items, weight_per_item, value_per_item):
 
         self.wa = weight_available
+        self.ni = number_of_items
         self.wpi = weight_per_item
         self.vpi = value_per_item
-
-    @property
-    def number_of_items(self):
-        return len(self.wpi)
 
     @lru_cache(maxsize=None)
     def rec_knap(self, iterator, available_weight):
@@ -43,7 +40,7 @@ class Knapsack:
         """
         j = self.wa
         values_used = []
-        for i in range(self.number_of_items, 0, -1):
+        for i in range(self.ni, 0, -1):
             if self.rec_knap(i, j) != self.rec_knap(i-1, j):
                 # appending items used to values_used list
                 values_used.append((self.wpi[i-1], self.vpi[i-1]))
@@ -52,7 +49,7 @@ class Knapsack:
         # reversing list to display items in order given
         values_used = values_used[::-1]
 
-        return self.rec_knap(self.number_of_items, self.wa), values_used
+        return self.rec_knap(self.ni, self.wa), values_used
 
     @classmethod
     def process_input_data(cls, input_data):
@@ -60,24 +57,26 @@ class Knapsack:
         Validates and processes input data. Returns list of items ready for
         object initailzation.
         """
-        data = [line.replace(' ', '').strip() for line in input_data]
+        data = [line.strip() for line in input_data]
 
-        if len(data) != 3:
+        if len(data) != 4:
             # file must be 3 lines long
             raise ValueError(cls._DATA_LENGTH_ERROR.format(len(data)))
-
-        if len(data[0]) != 1 or len(data[1]) != len(data[2]):
-            raise ValueError(cls._DATA_INPUT_ERROR)
 
         try:
             data_list = []
             # casting values to integers and appending them to data_list
             for element in data:
-                if len(element) == 1:
+                if element in data[:2]:
                     data_list.append(int(element))
                 else:
-                    data_list.append([int(char) for char in element])
+                    data_list.append([int(char) for char in element.split(' ')])
+
+            if data_list[1] != len(data_list[2]) != len(data_list[3]):
+                raise ValueError(cls._DATA_INPUT_ERROR)
+
             return data_list
+
         except Exception as ex:
             raise ValueError(str(ex))
 
@@ -109,12 +108,14 @@ if __name__ == '__main__':
 
 """
 input file:
-7               - maximum weight
-2 3 4 5         - weight of items
-3 4 5 5         - items value
+190                  - maximum weight
+6                    - number of items
+56 59 80 64 75 17    - weight of items
+50 50 64 46 50 5     - items value
 
 result:
-9               - max value produced
-3 4             - used items listed
-4 5               with weight - value pairs
+150                  - max value produced
+56 50                - used items listed
+59 50                  with weight - value pairs
+75 50
 """
